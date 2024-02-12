64b3db73a0a8ff5cffa753b697a3f22fc76c67dc
Carson Brase <CarsonBrase@wifi-124-0-207-westmont.edu>
2024-02-12 13:51:43 -0800

    Output DB models to webpage, formatted w bootstrap

--- listings/models.py
+++ listings/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 
 class MontUser(models.Model):
+    DORM_CHOICES = [
+        ('page', 'Page'),
+        ('emerson', 'Emerson'),
+        ('clark', 'Clark'),
+        ('vk', 'VK'),
+        ('armington', 'Armington'),
+        ('glc', 'GLC'),
+        ('off campus', 'Off Campus'),
+    ]
+
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
     phone = models.CharField('User Phone', max_length=25)
     email = models.EmailField('User Email')
-    dorm = models.CharField(max_length=50)
+    dorm = models.CharField(max_length=50, choices=DORM_CHOICES)
     
     def __str__(self):
         return self.first_name + ' ' + self.last_name
@@ -21,12 +31,15 @@ class Listing(models.Model):
         ('not_great', 'Not Great'),
         ('bad', 'Bad'),
     ]
+
+    DEFAULT_IMAGE_PATH = 'static/listings/default_image.jpeg'
+
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     seller = models.ForeignKey(MontUser, on_delete=models.CASCADE)
     condition = models.CharField(max_length=20, choices=CONDITION_CHOICES)
-    photo = models.ImageField(upload_to='listings/', null=True, blank=True)
+    photo = models.ImageField(upload_to='listings/', default=DEFAULT_IMAGE_PATH, null=True, blank=True)
     is_active = models.BooleanField()
     date_created = models.DateField('Date Posted')
 
