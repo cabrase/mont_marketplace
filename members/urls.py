b014dde02cfa1cac4764d83c2dc2686cc632fe74
Carson Brase <CarsonBrase@wifi-90-14-24-westmont.edu>
2024-04-22 15:28:53 -0700

    Media handling in urls

--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('login_user', views.login_user, name='login'),
@@ -9,4 +11,4 @@ urlpatterns = [
     path('verify-email/done/', views.verify_email_done, name='verify-email-done'),
     path('verify-email-confirm/<uidb64>/<token>/', views.verify_email_confirm, name='verify-email-confirm'),
     path('verify-email/complete/', views.verify_email_complete, name='verify-email-complete'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
