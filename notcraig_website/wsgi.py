a49fb68e871b04004f73b5e4f6fdc5f7ff80ac04
Carson Brase <CarsonBrase@wifi-90-137-232-westmont.edu>
2024-04-24 12:37:28 -0700

    storages issue still

--- notcraig_website/wsgi.py
+++ notcraig_website/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notcraig_website.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notcraig_website.settings')
 
 application = get_wsgi_application()
 
